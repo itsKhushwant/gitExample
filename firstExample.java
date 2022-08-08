8087749bf98cec66badcfc6d6315fcbc724e7c8a
khushwant singh <khushwant@gmail.com>
2022-08-09 00:55:53 +0530

    this is to just check the github data updation

--- firstExample.java
+++ firstExample.java
@@ -2,6 +2,8 @@ public class firstExample {
 
     public static void main(String[] args) {
         System.out.println("just smile"+"this also gonna pass");
+		System.out.println("this is to check the new insertion into"
+							+"the Github");
     }
 
 }
